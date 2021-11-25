4e6b34d61cd188950a927a16ab1b26aa29b35582
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-11-25 18:31:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     defaultLocale: "en-US",
   },
   purge: {
-    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx", './public/*.html'],
     // These options are passed through directly to PurgeCSS
   },
   theme: {
